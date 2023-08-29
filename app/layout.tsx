32f1c51577b3a96f0da440d49d4133759db0b090
tendai-gatahwa <107326659+tendai-gatahwa@users.noreply.github.com>
2023-08-29 16:55:50 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,8 +4,8 @@ import ActiveStatus from './components/ActiveStatus'
 import ToasterContext from './context/ToasterContext'
 
 export const metadata = {
-  title: 'Messenger',
-  description: 'Messenger Clone',
+  title: 'MSU-Messanger'',
+  description: 'A Social Nextwork For MSU Students',
 }
 
 export default function RootLayout({
